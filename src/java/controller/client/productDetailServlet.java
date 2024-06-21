d6c1aca37255b2a06d8940bb5f149afd36f41512
NGUYENTRONGTAN553 <117750303+NGUYENTRONGTAN553@users.noreply.github.com>
2024-06-21 22:02:19 +0700

    Create productDetailServlet.java

---
+++ src/java/controller/client/productDetailServlet.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller.client;
+
+import dao.ProductDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+import model.Product;
+
+/**
+ *
+ * @author hc
+ */
+@WebServlet(name="productDetailServlet", urlPatterns={"/productDetailServlet"})
+public class productDetailServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet productDetailServlet</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet productDetailServlet at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+//        processRequest(request, response);
+        int ma_sanpham = Integer.parseInt(request.getParameter("id"));
+        ProductDao spd = new ProductDao();
+        Product sp = spd.getmaProduct(ma_sanpham);
+        request.setAttribute("product", sp);
+
+        List<Product> s = spd.getNewProduct();
+        request.setAttribute("spss", s);
+        request.getRequestDispatcher("./views/client/home/productDetail.jsp").forward(request, response);
+    } 
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
