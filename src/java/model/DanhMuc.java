7bc203a74a6f96b5297e27a589a0034af43ec829
NGUYENTRONGTAN553 <117750303+NGUYENTRONGTAN553@users.noreply.github.com>
2024-06-21 22:02:46 +0700

    Create DanhMuc.java

---
+++ src/java/model/DanhMuc.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author hc
+ */
+public class DanhMuc {
+    private int id_danhmuc;
+    private String ten_danhmuc;
+    private String mo_ta;
+
+    public DanhMuc(int id_danhmuc, String ten_danhmuc, String mo_ta) {
+        this.id_danhmuc = id_danhmuc;
+        this.ten_danhmuc = ten_danhmuc;
+        this.mo_ta = mo_ta;
+    }
+
+    // Các phương thức get và set
+}
